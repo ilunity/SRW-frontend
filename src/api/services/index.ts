117352682ff2e0c32af2dc077dee1f25c3fc885c
Ilya Shishkin <mostbook@mail.ru>
2023-09-20 10:07:36 +0700

    Bugfix. Fixed incorrect imports.

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -2,5 +2,4 @@ export * from './auth.service';
 export * from './cookie.service';
 export * from './categories.service';
 export * from './products.service';
-export * from './user.service';
 export * from './recipes.service';
